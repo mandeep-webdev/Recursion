7a89ea7327e1883e7073f27cebb74bc82f1f7944
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2023-03-11 08:01:05 +0000

    array with recursion

--- ArrayQuestions/findAllIndexes2.js
+++ ArrayQuestions/findAllIndexes2.js
@@ -1,23 +1,23 @@
-const arr = [2,3,9,0,9]
-function findAll(arr,target,index) {
-debugger
-   const foundEl = []
-  
+
+function findAll(arr, target, index) {
+
+  const foundEl = []
+
   // base condition
-  if(index === arr.length) {
+  if (index === arr.length) {
     return foundEl
   }
- 
-  if(arr[index] === target) {
+
+  if (arr[index] === target) {
     foundEl.push(index)
   }
-  
-  const ansFromBelowCalls = findAll(arr,target,index+1)
+
+  const ansFromBelowCalls = findAll(arr, target, index + 1)
 
   foundEl.push(...ansFromBelowCalls)
   return foundEl
-  
+
 }
 
-const ans = findAll(arr,9,0)
+const ans = findAll([2, 3, 9, 0, 9], 9, 0)
 console.log(ans)
